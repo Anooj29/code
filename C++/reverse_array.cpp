76133389c3de6bf21a5138a69faa87609dd85061
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2024-10-24 01:38:53 +0530

    commit

--- C++/reverse_array.cpp
+++ C++/reverse_array.cpp
@@ -1,23 +1,20 @@
-#include<iostream>
-
+#include <iostream>
 int main(){
-    
-    int n;
-    std::cout<<"Enter number of elements in array:"<<std::endl;
-    std::cin>>n;
 
+    int n;
+    std::cout<<"Enter number of elements: "<<std::endl;
+    std::cin>>n;
     int array[n];
 
-    std::cout<<"Enter elements of array:"<<std::endl;
-
-    for(int i = 0 ; i < n ; i++){
+    std::cout<<"Enter elements: "<<std::endl;
+    
+    for (int i = 0 ; i < n ; i++){
         std::cin>>array[i];
     }
 
-    std::cout<<"The reverse array is: \n";
+    std::cout<<"The reverse of array is: "<<std::endl;
 
-    for(int i = n - 1 ; i >= 0 ; i--){
-        std::cout<<array[i]<<std::endl;
+    for (int i = n - 1 ; i >= 0 ; i--){
+        std::cout<<array[i]<<"\n";
     }
-
 }
