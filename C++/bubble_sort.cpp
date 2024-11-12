86976d0bd268284e372dd7d33e3d18439343d2d1
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2024-11-12 13:58:02 +0530

    Cpp

--- C++/bubble_sort.cpp
+++ C++/bubble_sort.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
-void bubblesort(vector<int> & arr){
+void BubbleSort(vector<int> & arr){
     int n = arr.size();
     bool swapped;
 
     for(int i = 0 ; i < n - 1 ; i++){
         swapped = false;
-        for(int j = 0 ; j < n - i - 1 ; j++){
-            if (arr[j] > arr[j + 1]){
+
+        for(int j = 0 ; j < n - 1 - i ; j++){
+            if(arr[j]<arr[j+1]){
                 swap(arr[j],arr[j+1]);
                 swapped = true;
             }
         }
         if(!swapped){
-            break;
-        }
+                break;
+            }
     }
 }
 
 void privatevector(const vector<int> & arr){
-    for(int num : arr){
+    for (int num : arr){
         cout<<" "<<num;
     }
 }
 
 int main(){
-    vector<int> arr = {64, 34, 25, 78, 39, 45};
-    bubblesort(arr);
-    cout<<"Sorted array: \n";
+    vector<int> arr = {35, 3, 54, 23, 654, 64, 23, 1, 38};
+    BubbleSort(arr);
+    cout<<"sorted array \n";
     privatevector(arr);
     return 0;
 }
