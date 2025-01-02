ac48cc8d36fe186aedba71fb985391554ba314c4
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2025-01-02 20:38:45 +0530

    commit

--- R/Hello.R
+++ R/Hello.R
@@ -1,15 +1,3 @@
-# Basic arithmetic operations
-a <- 10  # Assign 10 to variable 'a'
-b <- 5   # Assign 5 to variable 'b'
+#First Program
 
-# Perform operations
-sum <- a + b
-difference <- a - b
-product <- a * b
-quotient <- a / b
-
-# Print results
-print(paste("Sum:", sum))
-print(paste("Difference:", difference))
-print(paste("Product:", product))
-print(paste("Quotient:", quotient))
+print("Hello World")
---
+++ R/Operations.R
@@ -0,0 +1,15 @@
+# Basic arithmetic operations
+a <- 10  
+b <- 5   
+
+# Perform operations
+sum <- a + b
+difference <- a - b
+product <- a * b
+quotient <- a / b
+
+# Print results
+print(paste("Sum:", sum))
+print(paste("Difference:", difference))
+print(paste("Product:", product))
+print(paste("Quotient:", quotient))
