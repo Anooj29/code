c665f6cd2b9d1eddd0b2800175c5291f62788499
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2025-01-02 23:53:18 +0530

    commit

---
+++ R/graph.r
@@ -0,0 +1,8 @@
+# Generate a sequence of numbers
+x <- 1:10
+y <- x^2  # Square of each element in x
+
+# Create a plot
+plot(x, y, type = "b", col = "blue", 
+     main = "Simple Plot Example",
+     xlab = "X Values", ylab = "Y Values")
