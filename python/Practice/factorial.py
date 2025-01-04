46864381ea04886a63ad11961c92fd608ddf224f
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2025-01-04 23:58:36 +0530

    commit

---
+++ python/Practice/factorial.py
@@ -0,0 +1,14 @@
+# Code to find factorial of any number
+
+num = int(input("Enter number: "))
+factorial = int 
+
+if (num < 0):
+    print("Do not enter negative number")
+elif(num == 0):
+    print("factorial of number is: 1")
+
+
+for i in range(1, num+1):
+    factorial = factorial * i
+    print(factorial)
